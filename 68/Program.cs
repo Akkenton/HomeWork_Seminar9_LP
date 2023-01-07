69bf92fc843caa298781514edf440c3125470da7
Akkenton <akkenton.msk@gmail.com>
2023-01-07 10:19:33 +0300

    Решение задачи №68

--- 68/Program.cs
+++ 68/Program.cs
@@ -2,3 +2,17 @@
 
 // m = 2, n = 3 -> A(m,n) = 9  
 // m = 3, n = 2 -> A(m,n) = 29
+
+
+int Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m != 0 && n == 0) return Akkerman(m - 1, 1);
+    if (m > 0 && n > 0) return Akkerman(m - 1, Akkerman(m, n - 1));
+    return Akkerman(m, n);
+}
+
+Console.Clear();
+int m = 3;
+int n = 2;
+System.Console.WriteLine(Akkerman(m, n));
