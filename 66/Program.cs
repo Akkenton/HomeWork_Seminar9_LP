c1b16ee80e9904037f6f16f46589ffe0481cf740
Akkenton <akkenton.msk@gmail.com>
2023-01-07 10:07:55 +0300

    Решение задачи №66

--- 66/Program.cs
+++ 66/Program.cs
@@ -2,3 +2,20 @@
 
 // M = 1; N = 15 -> 120  
 // M = 4; N = 8. -> 30
+
+int ResultSummRecurent(int M, int N, int result)
+{
+    if (N == M) return result;
+    else
+    {
+        result += N;
+        return ResultSummRecurent(M, N - 1, result);
+    }
+}
+
+
+Console.Clear();
+int M = 1;
+int N = 15;
+int result = M;
+System.Console.WriteLine(ResultSummRecurent(M, N, result));
