7e9e4bb0abbc0094384bda494947bc8dc4a0369f
Akkenton <akkenton.msk@gmail.com>
2023-01-07 09:48:41 +0300

    Решение задачи №64

--- 64/Program.cs
+++ 64/Program.cs
@@ -2,3 +2,21 @@
 
 // N = 5 -> "5, 4, 3, 2, 1"  
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+int ReverseRecurentN(int N)
+{
+    if (N == 1) return N;
+    else
+    {
+        System.Console.Write($"{N}, ");
+        N--;
+        return ReverseRecurentN(N);
+    }
+}
+
+Console.Clear();
+int N = 8;
+System.Console.Write("'");
+System.Console.Write(ReverseRecurentN(N));
+System.Console.Write("'");
